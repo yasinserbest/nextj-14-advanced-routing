52d3852e9d1d352986261f43577b86c795bf5ff0
YASIN SERBEST <yasinserbest7@icloud.com>
2024-05-18 17:01:38 +0300

    conditional login slot is added

---
+++ src/app/images/@login/page.tsx
@@ -0,0 +1,7 @@
+import Card from '@/components/card/card';
+
+const Login = () => {
+  return <Card>Loggedin succesfully </Card>;
+};
+
+export default Login;
--- src/app/images/layout.tsx
+++ src/app/images/layout.tsx
@@ -3,13 +3,16 @@ export default function ComplexDashboardLayout({
   animals, //slots are available as props so you don't have to import them manually
   furniture,
   plants,
+  login,
 }: {
   children: React.ReactNode;
   animals: React.ReactNode;
   furniture: React.ReactNode;
   plants: React.ReactNode;
+  login: React.ReactNode;
 }) {
-  return (
+  const isLoggedIn = false;
+  return isLoggedIn ? (
     <div>
       <h1>{children}</h1>
       <div style={{ display: 'flex' }}>
@@ -20,5 +23,7 @@ export default function ComplexDashboardLayout({
         <div style={{ display: 'flex', flex: 1 }}>{plants}</div>
       </div>
     </div>
+  ) : (
+    login
   );
 }
